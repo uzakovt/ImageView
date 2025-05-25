928bb1bc819516ec24644346dae69ed415a30319
Temurbek Uzakov <uzakovv.t@gmail.com>
2025-05-26 01:44:41 +0400

    Sprint 8 tasks

---
+++ ImageView/Model/ImageListCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ImageListCell: UITableViewCell {
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var buttonView: UIButton!
+    @IBOutlet weak var dateLabelView: UILabel!
+    static let reuseIdentifier = "ImageListCell"
+    
+}
