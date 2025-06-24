e76a507533834fad51e9f7588f4fe6f27856a170
Temurbek Uzakov <uzakovv.t@gmail.com>
2025-06-24 17:56:53 +0400

    Uploading current version before code wrapping the profile screen

--- ImageView/Controller/SingleImageViewController.swift
+++ ImageView/Controller/SingleImageViewController.swift
@@ -10,13 +10,76 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet{
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
+
         }
     }
+    private var activityVC: UIActivityViewController?
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var imageView: UIImageView!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        guard let image else { return }
+        activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
     }
+    
+    @IBAction private func shareButtonPressed(_ sender: Any) {
+        guard let activityVC else { return }
+        activityVC.popoverPresentationController?.sourceView = self.view
+        present(activityVC, animated: true)
+    }
+    
+    @IBAction private func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        
+        //Centering the Image
+        var frameToCenter = imageView.frame
+        let widthDiff  = visibleRectSize.width  - frameToCenter.size.width
+        let heightDiff = visibleRectSize.height - frameToCenter.size.height
+        frameToCenter.origin.x = (widthDiff  > 0) ? widthDiff  / 2 : 0;
+        frameToCenter.origin.y = (heightDiff > 0) ? heightDiff / 2 : 0;
+        imageView.frame = frameToCenter;
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let boundsSize  = scrollView.bounds.size
+        var frameToCenter = imageView.frame
+
+        // center horizontally and vertically
+        let widthDiff  = boundsSize.width  - frameToCenter.size.width
+        let heightDiff = boundsSize.height - frameToCenter.size.height
+        frameToCenter.origin.x = (widthDiff  > 0) ? widthDiff  / 2 : 0;
+        frameToCenter.origin.y = (heightDiff > 0) ? heightDiff / 2 : 0;
+
+        imageView.frame = frameToCenter;
+    }
+    
 }
